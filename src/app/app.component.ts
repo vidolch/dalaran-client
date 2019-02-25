71e66c3fa6a2478a7b48ead1d0df9bbaf4c7d5ef
Vidol Chalamov <vidol.chalamov@primeholding.com>
2019-02-25 16:39:26 +0200

    Added authentication using oicd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
+import { authConfig } from './auth.config';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,50 @@ import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
 })
 export class AppComponent {
   title = 'app';
+  userProfile: object;
+
+  constructor(private oauthService: OAuthService) {
+    this.configureWithNewConfigApi();
+  }
+
+  loadUserProfile(): void {
+    this.oauthService.loadUserProfile().then(up => (this.userProfile = up));
+  }
+
+  public login() {
+      this.oauthService.initImplicitFlow();
+  }
+
+  public logout() {
+      this.oauthService.logOut();
+  }
+
+  public get name() {
+      const claims: object = this.oauthService.getIdentityClaims();
+      if (!claims) {
+        return null;
+      }
+
+      return claims['preferred_username'];
+  }
+
+  private configureWithNewConfigApi() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+
+    // Load Discovery Document and then try to login the user
+    this.oauthService.loadDiscoveryDocument().then(doc => {
+      this.oauthService.tryLogin();
+      this.oauthService.loadUserProfile().then(up => (this.userProfile = up));
+    });
+
+    this.oauthService.events
+      .pipe(filter(e => e.type === 'token_expires'))
+      .subscribe(e => {
+        // tslint:disable-next-line:no-console
+        console.debug('received token_expires event', e);
+        this.oauthService.silentRefresh();
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MaterialUimoduleModule } from './material-uimodule.module';
 import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { OAuthModule } from 'angular-oauth2-oidc';
+
 import { CollectionComponent } from './collection/collection.component';
 import { CollectionListComponent } from './collection/collection-list/collection-list.component';
 import { CollectionService } from './collection/collection.service';
@@ -18,6 +20,9 @@ import { ResourceListComponent } from './resource/resource-list/resource-list.co
 import { ResourceService } from './resource/resource.service';
 import { ResourceDetailsComponent } from './resource/resource-details/resource-details.component';
 import { ResourceCreateComponent } from './resource/resource-create/resource-create.component';
+import { RequestListComponent } from './request/request-list/request-list.component';
+import { RequestCreateComponent } from './request/request-create/request-create.component';
+import { RequestDetailsComponent } from './request/request-details/request-details.component';
 
 
 @NgModule({
@@ -29,7 +34,10 @@ import { ResourceCreateComponent } from './resource/resource-create/resource-cre
     CollectionDetailsComponent,
     ResourceListComponent,
     ResourceCreateComponent,
-    ResourceDetailsComponent
+    ResourceDetailsComponent,
+    RequestListComponent,
+    RequestCreateComponent,
+    RequestDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(
@@ -40,7 +48,13 @@ import { ResourceCreateComponent } from './resource/resource-create/resource-cre
     BrowserAnimationsModule,
     MaterialUimoduleModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://localhost:5050'],
+        sendAccessToken: true
+      }
+    })
   ],
   providers: [
     CollectionService,
